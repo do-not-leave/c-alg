3f1c1e3c7fd9786e9a09a24f2a4bfa73dd707c7e
2lovecode <liu546hao@163.com>
2020-05-15 17:55:07 +0800

    2020-05-18作业

---
+++ c/2020-05-18/liuh31/reverse.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+#define LINE_MAX 100
+
+void reverse(char line[]);
+
+int main() {
+    char c;
+    char line[LINE_MAX];
+    int len = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            line[len+1] = '\0';
+            printf("原字符串:%s\n", line);
+            reverse(line);
+            len = 0;
+        } else {
+            line[len++] = c;
+        }
+    }
+
+    return 0;
+}
+
+void reverse(char line[]) {
+    int i = 0, j = 0;
+    char t;
+    while(line[++j] != '\0') {
+    }
+    printf("%d\n", j);
+    
+    printf("反转字符串:%s\n", line);
+}
