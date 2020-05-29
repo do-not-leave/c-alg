7fa7586119f0336907dac08131793a8b6b04a45a
wangwt1 <wangwt1@ziroom.com>
2020-05-29 15:42:03 +0800

    修改

--- c/2020-05-29/wangwt1/6th.c
+++ c/2020-05-29/wangwt1/6th.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-
 void itob(int n, char s[], unsigned b)
 {
+    //确定字符串位数
     int x = 0;
     while (n > pow(b, x))
     {
         x++;
     }
     printf("位数:%d\n", x);
+
     int mod;
-    for(x -= 1; x >= 0; x--) {
+    //字符串末尾
+    s[x] = '\0';
+    for (x = x-1; x >= 0; x--) {
         mod = n % b;
         if (mod < 10) {
             s[x] = mod + '0';
@@ -19,16 +22,17 @@ void itob(int n, char s[], unsigned b)
             s[x] = mod - 10 + 'A';
         }
         n /= b;
-        printf("当前值%d,余数%d\n", n, mod);
+        printf("当前值x %d, n %d, 余数%d\n", x, n, mod);
     }
 }
 
 int main()
 {
     char s[20];
-    int num = 734;
-    itob(num, s, 16);
-    printf("输入 %d, 结果 %s\n", num, s);
+    int num = 10;
+    unsigned base = 16;
+    itob(num, s, base);
+    printf("输入%d，转换为%d进制，结果 %s\n", num, base, s);
 
 }
 
