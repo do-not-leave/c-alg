46085795fc684b950720b7fb5da412abbd15f678
liuhao <liu546hao@163.com>
2020-06-13 23:57:30 +0800

    qsort

---
+++ c/2020-06-15/liuh31/qsort.c
@@ -0,0 +1,16 @@
+#include "./qsort.h"
+
+
+void qsort()
+{
+
+}
+
+void printIntArray(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d,", array[i]);
+  }
+  printf("\n");
+}
---
+++ c/2020-06-15/liuh31/test.c
@@ -0,0 +1,7 @@
+#include "./qsort.h"
+
+int main() {
+  int data[6] = {1, 2, 3, 4, 5, 6};
+  printIntArray(data, 6);
+  return EXIT_SUCCESS;
+}
