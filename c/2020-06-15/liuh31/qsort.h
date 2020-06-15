8a5873ae26abc158209b2180fd75b58ec3c1746a
2lovecode <liu546hao@163.com>
2020-06-15 17:11:10 +0800

    qsort opt

--- c/2020-06-15/liuh31/qsort.h
+++ c/2020-06-15/liuh31/qsort.h
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define QSORT_MIN_NUM 3
+#define ARRAY_MAX_NUM 1000
+
 void swap_int(int *a, int *b);
 void m_qsort(int *a, int left, int right);
+void m1_qsort(int *a, int left, int right);
+void m_insert_sort(int *a, int left, int right);
 void print_int_array(int *a, int size);
--- c/2020-06-15/liuh31/test.c
+++ c/2020-06-15/liuh31/test.c
@@ -1,9 +1,55 @@
 #include "./qsort.h"
 
 int main() {
-  int data[6] = {6, 2, 3, 5, 4, 1};
-  print_int_array(data, 6);
-  m_qsort(data, 0, 5);
-  print_int_array(data, 6);
+  int len = 0;
+  int data[ARRAY_MAX_NUM];
+  int sort_type = 1;
+
+  int start_flag = 1;
+
+  while (start_flag) {
+    start_flag = 0;
+    printf("input length: ");
+    if (scanf("%d", &len)) {
+      printf("input data: ");
+      for (int i = 0; i < len; i++) {
+        if (!scanf("%d", &data[i])) {
+          goto STOP;
+          break;
+        }
+      }
+      printf("input sort type: ");
+      if (!scanf("%d", &sort_type)) {
+        sort_type = 1;
+      }
+      switch (sort_type) {
+        case 1:
+          m_qsort(data, 0, len-1);
+          break;
+        case 2:
+          m1_qsort(data, 0, len-1);
+          break;
+        default:
+          m_qsort(data, 0, len-1);
+      }
+
+      printf("output data: ");
+      print_int_array(data, len);
+      printf("input 1 to continue: ");
+      if (!scanf("%d", &start_flag)) {
+        start_flag = 0;
+        goto STOP;
+        break;
+      }
+      printf("\n\n");
+    } else {
+      goto STOP;
+      break;
+    }
+  }
+
+STOP:
+  printf("\nstop\n");
+  
   return EXIT_SUCCESS;
 }
