8f2fbca419da4436cbafd1324ed701d435d6f65a
2lovecode <liu546hao@163.com>
2020-06-24 18:19:39 +0800

    aa

--- c/2020-06-24/liuh31/main.c
+++ c/2020-06-24/liuh31/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+#define MAXLINE 1000
+
+int main(int argc, char *argv[])
 {
-    return EXIT_SUCCESS;
+    int lineNo = 0;
+    char *lines[MAXLINE];
+    char *p;
+    char *stop = "stop";
+
+    char *tailLine;
+    int tailLineNum = 0;
+
+    char *line = NULL;
+
+    size_t mem_len = 0;
+    ssize_t line_len;
+    
+    if (argc != 3) {
+        return EXIT_FAILURE;
+    }
+
+    tailLine = argv[2];
+    
+    tailLineNum = atoi(tailLine);
+
+    while ((line_len = getline(&line, &mem_len, stdin)) != -1) {
+        if (strncmp(line, stop, 4) == 0) {
+            printf("\n");
+            break;
+        }
+        p = malloc(MAXLINE);
+        strcpy(p, line);
+        lines[lineNo++] = p;
+    }
+
+    for (int i = 0; i < lineNo; i++) {
+        printf("%s", lines[i]);
+    }
 }
