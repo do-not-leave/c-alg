ed33de66dd950a963059fa33caa2770898b2c600
2lovecode <liu546hao@163.com>
2020-07-20 16:52:12 +0800

    zs

--- c/2020-07-15/liuh31/m_zset.c
+++ c/2020-07-15/liuh31/m_zset.c
@@ -4,12 +4,14 @@
 #define MAX_LEVEL 20
 #define SKIPLIST_P  0.25
 
+typedef struct SkipListLevel {
+    struct SkipListNode *forward;
+} SkipListLevel;
+
 typedef struct SkipListNode {
     int value;
     struct SkipListNode *backward;
-    struct SkipListLevel {
-        struct SkipListNode *forward;
-    } level[];
+    SkipListLevel level[];
 } SkipListNode;
 
 typedef struct SkipList {
@@ -48,17 +50,84 @@ int RandomLevel(void) {
     return (level < MAX_LEVEL) ? level : MAX_LEVEL;
 }
 
-void Insert(SkipList *skipL, int value) {
-    SkipListNode *update[MAX_LEVEL], *tmp;
+//添加一个节点
+SkipListNode *Insert(SkipList *skipL, int value) {
+    SkipListNode *update[MAX_LEVEL], *tmp, *node;
+    int level, i;
 
-    for (int i = skipL->totalLevel - 1; i >= 0; i--) {
+    tmp = skipL->head;
+    for (i = skipL->totalLevel - 1; i >= 0; i--) {
         while (tmp->level[i].forward && tmp->level[i].forward->value < value) {
             tmp = tmp->level[i].forward;
         }
         update[i] = tmp;
     }
+
+    level = RandomLevel();
+
+    if (level > skipL->totalLevel) {
+        for (i = skipL->totalLevel; i < level; i++) {
+            update[i] = skipL->head;
+        }
+        skipL->totalLevel = level;
+    }
+
+    node = CreateSkipListNode(level, value);
+    for (i = 0; i < level; i++) {
+        node->level[i].forward = update[i]->level[i].forward;
+        update[i]->level[i].forward = node;
+    }
+
+    node->backward = (update[0] == skipL->head) ? NULL : update[0];
+
+    if (node->level[0].forward) {
+        node->level[0].forward->backward = node;
+    } else {
+        skipL->tail = node;
+    }
+    return node;
 }
 
-void Find(SkipList *skipL, int value) {
 
+//查找
+int Find(SkipList *skipL, int value) {
+    int i;
+    SkipListNode *start, *stop;
+
+    start = skipL->head;
+    for (i = skipL->totalLevel - 1; i >= 0; i--) {
+        while (start->level[i].forward && start->level[i].forward->value < value) {
+            start = start->level[i].forward;
+        }
+        stop = start->level[i].forward;
+    }
+
+    if (start->value == value) {
+        return 1;
+    }
+    while (start != stop) {
+        if (start->level[0].forward->value == value) {
+            return 1;
+        }
+        start = start->level[0].forward;
+    }
+
+    return -1;
+}
+
+int main() {
+    SkipList *list;
+    int insertData[7] = {76, 90, 2, 4, 67, 1, 4};
+    int testData[5] = {4, 5, 6, 76, 1};
+
+    list = CreateSkipList();
+
+    for (int i = 0; i < 7; i++) {
+        Insert(list, insertData[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d : %d\n", testData[i], Find(list, testData[i]));
+    }
+    
 }
